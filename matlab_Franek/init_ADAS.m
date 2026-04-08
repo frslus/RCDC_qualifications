46308b4d41e2b83c3db9eb7b644d424bbc78d370
Franciszek Slusarczyk <frslusarczyk@student.agh.edu.pl>
2026-04-08 21:39:01 +0200

    repair TTC and prog_czasowy

--- matlab_Franek/init_ADAS.m
+++ matlab_Franek/init_ADAS.m
@@ -7,7 +7,7 @@ clear; clc;
 
 %% 1. Parametry początkowe symulacji
 v_ego = 30;        % [m/s] Prędkość początkowa naszego pojazdu
-v_lead = 25;       % [m/s] Prędkość pojazdu poprzedzającego 
+v_lead = 30;       % [m/s] Prędkość pojazdu poprzedzającego 
 d_init = 160;      % [m] Dystans początkowy.
 
 %% 2. Parametry czujników i filtru 
