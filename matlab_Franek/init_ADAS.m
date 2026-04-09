da3b008e85cd664d4722bf3358a78968cde307e1
Franciszek Slusarczyk <frslusarczyk@student.agh.edu.pl>
2026-04-10 00:49:03 +0200

    emergency_braking hotfix

--- matlab_Franek/init_ADAS.m
+++ matlab_Franek/init_ADAS.m
@@ -6,9 +6,9 @@ clear; clc;
 
 
 %% 1. Parametry początkowe symulacji
-v_ego = 40;        % [m/s] Prędkość początkowa naszego pojazdu
+v_ego = 80;        % [m/s] Prędkość początkowa naszego pojazdu
 v_lead = 25;       % [m/s] Prędkość pojazdu poprzedzającego 
-d_init = 160;      % [m] Dystans początkowy.
+d_init = 300;      % [m] Dystans początkowy.
 
 %% 2. Parametry czujników i filtru 
 radar_max_range = 150;  % [m] Zasięg przedniego radaru 
